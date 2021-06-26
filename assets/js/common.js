9e84fa18db892b7f51f09998897a85aadad45204
gitname <82920762+dsa02070@users.noreply.github.com>
2021-06-27 00:34:00 +0900

    Initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    // top btn hide
+    $(window).scroll(function () {
+        let scTop = $(this).scrollTop();
+        if (scTop <= 140) {
+            $('.top_btn').hide()
+        } else {
+            $('.top_btn').show()
+        }
+    })
+
+    // top btn event
+    $('.top_btn').click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 400)
+    })
+
+    // menu mouseenter event
+    $('header nav').mouseenter(function () {
+        $('.submenu').addClass('on')
+        $('.submenu_bg').addClass('on')
+    })
+    $('header').mouseleave(function () {
+        $('.submenu').removeClass("on");
+        $('.submenu_bg').removeClass('on')
+    })
+})
---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $(document).ready(function () {
+        $('.slick_wrap').slick({
+            autoplay: true,
+            autoplaySpeed: 4000,
+            arrows: true,
+        });
+    });
+
+})
