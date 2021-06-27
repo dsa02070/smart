eca2e9ac35da1d532dedac1230f83f8c1b35ea2e
gitname <82920762+dsa02070@users.noreply.github.com>
2021-06-28 00:14:15 +0900

    2021.06.28. section 영역 완성
    1. section 영역을 완료했습니다.
    2. slider의 경우 slick과 swiper를 사용했습니다.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,37 @@ $(function () {
         });
     });
 
+
+    var swiper01 = new Swiper(".slide01", {
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+
+    var swiper02 = new Swiper(".slide02", {
+        slidesPerView: 6,
+        spaceBetween: 24,
+        freeMode: true,
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+            },
+        },
+    });
+
 })
